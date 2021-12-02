c35d4577205f7fe005f7707f71571d8be936057c
Dan Yates <email@danielyates.co.uk>
2021-12-02 23:11:41 +0000

    Day two, part two

---
+++ day-two/part-two.js
@@ -0,0 +1,37 @@
+import fs from 'fs'
+
+const pilotCommandsInput = fs
+    .readFileSync('./day-two/input.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map((item) => {
+        const [command, value] = item.split(' ')
+        return { command, value: Number(value) }
+    })
+
+const performCommands = (pilotCommands) => {
+    let depth = 0
+    let horizontalPosition = 0
+    let aim = 0
+
+    for (const { command, value } of pilotCommands) {
+        switch (command) {
+            case 'forward':
+                horizontalPosition += value
+                depth = depth + aim * value
+                break
+            case 'down':
+                aim += value
+                break
+            case 'up':
+                aim -= value
+                break
+        }
+    }
+
+    return { depth, horizontalPosition }
+}
+
+const result = performCommands(pilotCommandsInput)
+console.log(result.horizontalPosition * result.depth)
